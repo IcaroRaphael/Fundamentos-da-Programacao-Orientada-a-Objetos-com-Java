7e26045bcf58b01a6cbd78296419ceaa28b4f221
IcaroRaphael <icaro2551@gmail.com>
2023-09-05 17:17:53 -0300

    feat: Pacotes

---
+++ Fundamentos-da-Programacao-Orientada-a-Objetos-com-Java/src/Pacotes/MinhaClasse.java
@@ -0,0 +1,16 @@
+package Pacotes;
+
+/*
+* Package versus Import:
+A localização de uma classe é definida pela palavra reservada package, logo, uma classe só contém, uma definição de
+package no arquivo, sempre na primeira linha do código. Para a utilização de uma classe existente em outros pacotes,
+necessitamos realizar a importação das mesmas, seguindo a recomendação abaixo:
+
+package
+
+import ...
+import ...
+*/
+
+public class MinhaClasse {
+}
